907e884b3b0c38c58be256d02f7ffb8c6cb9d616
SlavaAA666A <116429709+SlavaAA666A@users.noreply.github.com>
2022-11-22 22:24:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            System.Console.WriteLine("Введите имя и фамилию: ");
+            name = System.Console.ReadLine();
+            System.Console.WriteLine("Индивидуальный гороскоп для пользователя " + name + "!");
+            name = System.Console.ReadLine();
+            System.Console.WriteLine("Кем вы были в прошлой жизни:");
+            name = System.Console.ReadLine();
+            System.Console.WriteLine("Ваш знак зодиака - Телец, поэтому вы - тонко чувствующая натура.");
+
+
+
+        }
+    }
+}
