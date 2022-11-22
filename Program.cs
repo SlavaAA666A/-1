abaabeae60e314dafe821bf88aa626a8309a51b5
Vova221111 <118848971+Vova221111@users.noreply.github.com>
2022-11-22 22:35:29 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,11 +13,7 @@ namespace ConsoleApp4_2
             string name;
             System.Console.WriteLine("Введите имя и фамилию: ");
             name = System.Console.ReadLine();
-            System.Console.WriteLine("Индивидуальный гороскоп для пользователя " + name + "!");
-            name = System.Console.ReadLine();
-            System.Console.WriteLine("Кем вы были в прошлой жизни:");
-            name = System.Console.ReadLine();
-            System.Console.WriteLine("Ваш знак зодиака - Скорпион, поэтому вы - тонко чувствующая натура.");
+            System.Console.WriteLine("Ваш знак зодиака - Овен, поэтому вы - тонко чувствующая натура.");
 
 
 
